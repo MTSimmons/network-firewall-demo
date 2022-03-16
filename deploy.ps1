a71f590188a7cb5fd606d7a13db4f954ff88c978
Michael Simmons <Michael.Simmons@live.com>
2022-03-16 14:00:21 -0500

    Update deploy.ps1
    removes hard-coded subscription value

--- deploy.ps1
+++ deploy.ps1
@@ -1,5 +1,7 @@
 $StartDate = Get-Date -f 'yyyyMMdd-HHmmss'
 
-Select-AzSubscription -Subscription "df23c400-7a2f-4f25-bae4-0e5e26ea562e"
+# The Subscription is usually set in the Azure PowerShell session before running this. 
+#  or you can set it here (remove the comment and then either create a $SubscriptionId variable or replace the text here with the subscription id:)
+#    Select-AzSubscription -Subscription $SubscriptionId
 
 New-AzSubscriptionDeployment -Name "Deploy-$StartDate" -Location 'eastus' -TemplateFile ".\main.bicep" -TemplateParameterFile ".\main.params.json"
